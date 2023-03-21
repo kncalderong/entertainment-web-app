99598738ba3f95d89eaf889cdd0aa72de18ec42d
Kevin Nicolas <kncalderong@gmail.com>
2023-03-21 16:43:19 -0500

    feat: client folder and basic structure of vite react typescript with tailwind included

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="font-bold">
+      hello hello
+    </div>
+  )
+}
+
+export default App
