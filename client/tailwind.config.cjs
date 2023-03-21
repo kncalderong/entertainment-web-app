079af46e253057b573902570ef35775e1877af2b
Kevin Nicolas <kncalderong@gmail.com>
2023-03-21 17:49:06 -0500

    style: basic styles added

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -5,7 +5,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'red': '#FC4747',
+      'dark-blue': '#10141E',
+      'semi-dark-blue': '#161DF2',
+      'greyish-blue': '#5A698F',
+      'white': '#FFF'
+      },
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
