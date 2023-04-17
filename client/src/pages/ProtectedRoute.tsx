386e47ac72023775dfab315a646f36788d7bfa35
Kevin Nicolas <kncalderong@gmail.com>
2023-04-17 11:08:03 -0500

    feat: get current user function added in high shared component to avoid logout behaviour when reloading the page

--- client/src/pages/ProtectedRoute.tsx
+++ client/src/pages/ProtectedRoute.tsx
@@ -1,12 +1,45 @@
-import { Navigate } from 'react-router-dom';
-import { useAppSelector } from '../store/hooks';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { addUser } from '../features/user/userSlice';
+
+
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const user = useAppSelector((state) => state.user)
+  const [isUserLoading, setIsUserLoading] = useState<boolean>(true)
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
   
-  if (user.userId === "") {
-    return <Navigate to='/sign-in' />
+  const getCurrentUser = async () => {
+    const urlGetUser = "/api/v1/auth/getCurrentUser"
+    try {
+      const res = await axios.get(urlGetUser)
+      const user = res.data.user
+      dispatch(
+        addUser({ ...user })
+      )
+      setIsUserLoading(false)
+    } catch (error) {
+      setIsUserLoading(false)
+      navigate('/sign-in')
+      return
+    }
   }
+  useEffect(() => {
+    if (user.userId === "") {
+      getCurrentUser()
+    }
+    setIsUserLoading(false)
+  }, [])
+  
+  if (isUserLoading) {
+    return (
+      <div>Loading</div>
+    )
+  }
+  
   return children
 }
 
