6de28c56543176e7d30bd71e83228bcb5574da43
Kevin Nicolas <kncalderong@gmail.com>
2023-04-20 12:55:21 -0500

    feat: response for motionPictures expanded to 30 results per page according to figma design

--- controllers/motionPictureController.ts
+++ controllers/motionPictureController.ts
@@ -61,7 +61,7 @@ const getAllMotionPictures = async (req: RequestWithUser, res: Response) => {
   
    // setup pagination
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 24;
+  const limit = Number(req.query.limit) || 30;
   const skip = (page - 1) * limit;
 
   result = result.skip(skip).limit(limit);
