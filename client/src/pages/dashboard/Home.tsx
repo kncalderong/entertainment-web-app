464ed6da51f6370779df65d86a138a9683aac4cd
Kevin Nicolas <kncalderong@gmail.com>
2023-04-19 19:32:37 -0500

    feat: logic in home created to handle asynchronous search

--- client/src/pages/dashboard/Home.tsx
+++ client/src/pages/dashboard/Home.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import SearchBar from '../../components/SearchBar'
+import { useAppSelector } from '../../store/hooks'
+import Loading from '../../components/Loading'
+
 
 const Home = () => {
+  const isLoading = useAppSelector((state) => state.motionPictures.isLoading)
+
   return (
     <div>
-      Home
+      <SearchBar category="All" />
+      {isLoading ? <Loading/> : <div>Home</div> }
     </div>
   )
 }
