9974779216130fc454d719c22743d7565245391f
Kevin Nicolas <kncalderong@gmail.com>
2023-05-02 13:18:27 -0500

    feat: movies page finished

--- client/src/pages/dashboard/Movies.tsx
+++ client/src/pages/dashboard/Movies.tsx
@@ -1,10 +1,82 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../../store/hooks'
+import Loading from '../../components/Loading'
+import getMotionPictures from '../../utils/getMotionPictures'
+import { isLoadingMotionPictures, isSearchingMotionPictures, updateMotionPictures } from '../../features/motionPictures/motionPictureSlice'
+import { useNavigate } from 'react-router-dom'
+import { MotionPictureType } from '../../features/motionPictures/motionPictureSlice'
+import MotionPictureGrid from '../../components/MotionPictureGrid'
+import MotionPictureSlide from '../../components/MotionPictureSlide'
 
 const Movies = () => {
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+
+  const isLoading = useAppSelector((state) => state.motionPictures.isLoading)
+  const isSearching = useAppSelector((state) => state.motionPictures.isSearching)
+  const motionPictures = useAppSelector((state) => state.motionPictures.motionPictures)
+  const searchQuery = useAppSelector((state) => state.motionPictures.searchQuery)
+  const bookmarkedMotionPictures: string[] = useAppSelector((state) => state.user.bookmarks)
+
+  useEffect(() => {
+    if (!isSearching) {
+      (async () => {
+        dispatch(isLoadingMotionPictures({
+          isLoading: true,
+        }))
+        dispatch(isSearchingMotionPictures({
+          isSearching: false
+        }))
+        const motionPictures = await getMotionPictures("Movie")
+        if (motionPictures) {
+          dispatch(updateMotionPictures({
+            motionPictures: motionPictures.motionPictures,
+            numOfPages: motionPictures.numOfPages,
+            totalMotionPictures: motionPictures.totalMotionPictures
+          }))
+          dispatch(isLoadingMotionPictures({
+            isLoading: false,
+          }))
+        }
+        if (!motionPictures) {
+          dispatch(isLoadingMotionPictures({
+            isLoading: false,
+          }))
+          navigate('/sign-in')
+        }
+      })()
+    }
+  }, [isSearching])
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  if (!isLoading && isSearching) {
+    return (
+      <section className='px-4 pb-16 pt-6 w-full'>
+        <p className='text-white text-xl mb-6'>{`Found ${motionPictures.length} results for '${searchQuery}'`}</p>
+        <div className='flex flex-wrap gap-4'>
+          {motionPictures.map((motionPicture: MotionPictureType) => {
+            return (
+              <MotionPictureGrid motionPicture={motionPicture} key={motionPicture._id} isBookMarked={bookmarkedMotionPictures.includes(motionPicture._id)} />
+            )
+          })}
+        </div>
+      </section>
+    )
+  }
   return (
-    <div>
-      Movies
-    </div>
+    <section className='px-4 pb-16 pt-6'>
+      <p className='text-white text-xl mb-6'>Movies</p>
+      <div className='flex flex-wrap gap-4'>
+        {motionPictures.map((motionPicture: MotionPictureType) => {
+          return (
+            <MotionPictureGrid motionPicture={motionPicture} key={motionPicture._id} isBookMarked={bookmarkedMotionPictures.includes(motionPicture._id)} />
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
