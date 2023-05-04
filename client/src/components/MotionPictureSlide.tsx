fbde7bc18a6001e4b566063b0b1f3a594af15e55
Kevin Nicolas <kncalderong@gmail.com>
2023-05-04 17:55:27 -0500

    styke: tablet responsive handling of images in slider added

--- client/src/components/MotionPictureSlide.tsx
+++ client/src/components/MotionPictureSlide.tsx
@@ -4,15 +4,23 @@ import bookmarkedEmptyIcon from "../assets/icon-bookmark-empty.svg"
 import bookmarkedFullIcon from "../assets/icon-bookmark-full.svg"
 import movieMiniIcon from "../assets/icon-category-movie.svg"
 import tvSerieMiniIcon from "../assets/icon-category-tv.svg"
+import { useMediaQuery } from '../hooks/useMediaQuery'
 
 interface MotionPictureSlideProps {
   motionPicture: MotionPictureType
   isBookMarked: boolean
   bookmarkHandler: (motionPictureId: string) => void
 }
-const MotionPictureSlide = ({motionPicture, isBookMarked, bookmarkHandler} : MotionPictureSlideProps) => {
+const MotionPictureSlide = ({ motionPicture, isBookMarked, bookmarkHandler }: MotionPictureSlideProps) => {
+  
+  const isTablet = useMediaQuery('(min-width: 768px)')
+  const isDesktop = useMediaQuery('(min-width: 1024px)')
+  const style = {
+    backgroundImage: `linear-gradient(0deg, rgba(16, 20, 30, 0.2), rgba(16, 20, 30, 0.2)), url(${isDesktop ? motionPicture.thumbnail.regular.large : isTablet ? motionPicture.thumbnail.regular.medium : motionPicture.thumbnail.regular.small })`
+  }
+
   return (
-    <article className={`text-white relative min-w-[calc(100%-120px)] h-[140px] rounded-lg bg-cover flex flex-col justify-end p-4` } style={{backgroundImage: `linear-gradient(0deg, rgba(16, 20, 30, 0.2), rgba(16, 20, 30, 0.2)), url(${motionPicture.thumbnail.regular.small})`}}>
+    <article className={`text-white relative min-w-[calc(100%-120px)] h-[140px] rounded-lg bg-cover flex flex-col justify-end p-4 md:min-w-[calc(100%-250px)] md:h-[230px]` } style={style}>
       <div className='absolute top-[8px] right-[8px] bg-greyish-blue-opacity-50 p-[9px] rounded-full opacity-50 w-[32px] h-[32px] flex justify-center items-center' onClick={()=>bookmarkHandler(motionPicture._id)}>
         <img src={isBookMarked ? bookmarkedFullIcon : bookmarkedEmptyIcon} alt="bookmarkIcon" className='w-100' />
       </div>
