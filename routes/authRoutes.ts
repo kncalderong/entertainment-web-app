6bcbcaa1ff5e502f3b9be818eb9b9514cd60d90a
Kevin Nicolas <kncalderong@gmail.com>
2023-05-13 18:16:11 -0500

    feat: server API limit request when register and login by IP user inclued

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,5 +1,12 @@
 import express from 'express';
 const router = express.Router();
+import rateLimiter from 'express-rate-limit'
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // 10 request per 15 minutes
+  message: 'to many requests from this Ip address, please try again after 15 minutes'
+})
 
 import {
   register,
@@ -10,8 +17,8 @@ import {
 } from '../controllers/authController';
 import authenticateUser from '../middleware/auth';
 
-router.route('/register').post( register);
-router.route('/login').post(login);
+router.route('/register').post(apiLimiter, register);
+router.route('/login').post(apiLimiter, login);
 router.route('/logout').get(logout);
 router.route('/getCurrentUser').get(authenticateUser, getCurrentUser);
 router.route('/updateUser').patch(authenticateUser, updateUser);
