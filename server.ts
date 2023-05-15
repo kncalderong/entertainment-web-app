df118713fe78437e3dfe659c3480dd9dba6b4df2
Kevin Nicolas <kncalderong@gmail.com>
2023-05-15 16:28:41 -0500

    feat: adjustments added for deployment

--- server.ts
+++ server.ts
@@ -6,6 +6,11 @@ dotenv.config();
 //to handle errors throught middleware
 import 'express-async-errors'
 
+//to redirect front build to our server side rendering correct path
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 //to secure some general vulnerabilities that could have a backend
 import helmet from 'helmet';
 import mongoSanitize from 'express-mongo-sanitize';
@@ -24,6 +29,11 @@ import motionPictureRouter from './routes/motionPictureRoutes';
 import errorHandlerMiddleware from './middleware/error-handler';
 import notFoundMiddleware from './middleware/not-found';
 
+/* const __dirname = dirname(fileURLToPath(import.meta.url)); */
+
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, '../client/dist'))); //this middlewere allows access to all static files
+
 app.use(express.json()); //to allow reading body in json format
 app.use(cookieParser())
 
@@ -33,6 +43,12 @@ app.use(mongoSanitize());
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/motion-picture', motionPictureRouter)
+
+// only when ready to deploy
+app.get('*', (req, res) => { //this middleware allows me to handle every possible route in the front end through the index.html, but it always must be called after my server routes (the above ones)
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+});
+
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware);
